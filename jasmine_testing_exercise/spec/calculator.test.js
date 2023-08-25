ff37f1478ce423e7edc735986ec1e366c137224d
wftucker930 <willtucker930@gmail.com>
2023-08-25 12:40:17 -0400

    add new projects

---
+++ jasmine_testing_exercise/spec/calculator.test.js
@@ -0,0 +1,21 @@
+
+it('should calculate the monthly rate correctly', function () {
+  // ...
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+});
+
+/// etc
+
+describe('simple tests', () => {
+  it('should find true to be true', () => {
+    expect(true).toBe(true);
+  });
+  
+  it('should find false to be different from true', () => {
+    expect(false).not.toBe(true);
+  });
+})
